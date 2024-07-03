115eea3eff8cdabbc4be4cd4f56913c512d2b3ca
Liryck <Liryckbragin@gmail.com>
2024-07-04 01:55:40 +0300

    ended layot with styles for main page

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,51 @@
 $(document).ready(function(){
 
+	// Dropdown header submenu
+
+	const subMenu = document.querySelector('.header__submenu'),
+	      subMenuRelative = document.querySelector('.header__sub');
+
+	subMenuRelative.addEventListener('mouseover', (e) => {
+		subMenu.classList.add('header__submenu_active');
+	});
+
+	subMenu.addEventListener('mouseout', (e) => {
+		subMenu.classList.remove('header__submenu_active');
+	});
+
+	
 	// Mobile Menu
 
-	const mobMenu = document.querySelector('.header__mob-menu'),
-		  checkbox = document.getElementById('checkbox');
-
+	const mobMenu = document.querySelector('.mob-menu'),
+		  checkbox = document.getElementById('checkbox'),
+		  services = document.querySelector('[data-services]'),
+		  mobSubMenu = document.querySelector('.mob-menu__sublist'),
+		  mobServicesIcon = document.querySelector('.mob-menu__icon');
+		  
+	// Open mobile menu
 	checkbox.addEventListener('click', (e) => {
-		mobMenu.classList.toggle('header__mob-menu_active');
+		mobMenu.classList.toggle('mob-menu_active');
+	});
+
+	// Dropdown mobile submenu
+	services.addEventListener('click', (e) => {
+		mobSubMenu.classList.toggle('mob-menu__sublist_active');
+		mobServicesIcon.classList.toggle('mob-menu__icon_rotate');
 	});
 
 	
 	// Modal
 
-	$('[data-modal=consultation]').on('click', function() {
-		$('.overlay, #consultation').fadeIn('slow');
+	$('[data-modal=order]').on('click', function() {
+		$('.overlay, #order').fadeIn('slow');
 	});
 
-	$('[data-modal=order]').each(function(i) {
-		$(this).on('click', function() {
-			$('#order .modal__descr').text($('[data-title]').eq(i).text());
-			$('.overlay, #order').fadeIn('slow');
-		})
-	});
+	// $('[data-modal=order]').each(function(i) {
+	// 	$(this).on('click', function() {
+	// 		$('#order .modal__descr').text($('[data-title]').eq(i).text());
+	// 		$('.overlay, #order').fadeIn('slow');
+	// 	})
+	// });
 
 	$('.modal__close').on('click', function() {
 		$('.overlay, #consultation, #thx, #order').fadeOut('slow');
@@ -35,7 +59,7 @@ $(document).ready(function(){
 				name: {
 					required: true,
 					minlength: 3,
-					maxlength: 10
+					maxlength: 15
 				},
 				phone: "required"
 			},
@@ -60,7 +84,7 @@ $(document).ready(function(){
 
 	// Sending email
 
-	$('form').submit(function(e) {
+	$('form, form-modal').submit(function(e) {
 		e.preventDefault();
 
 		if (!$(this).valid()) {
@@ -73,13 +97,13 @@ $(document).ready(function(){
 			data: $(this).serialize()
 		}).done(function() {
 			$(this).find("input").val("");
-			$('#consultation, #order').fadeOut();
+			$('#order').fadeOut();
 			$('.overlay, #thx').fadeIn('slow');
 
 			$('form').trigger('reset');
 
 			dataLayer.push({
-				'event': 'ajaxFormSubmit', // Название события
+				'event': 'FormSubmit', // Название события
 				'formId': $form.attr('[data-id=form]'), // ID формы (или используйте другой уникальный идентификатор)
 			});
 		});
